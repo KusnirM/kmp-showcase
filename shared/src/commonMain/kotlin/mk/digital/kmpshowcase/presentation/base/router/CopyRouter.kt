732efbae4288c36fefb54e35a9ab6b8a346aa8ad
Miroslav Kusnir <mir.kusnir@gmail.com>
2026-01-22 04:29:43 +0100

    routers

---
+++ shared/src/commonMain/kotlin/mk/digital/kmpshowcase/presentation/base/router/CopyRouter.kt
@@ -0,0 +1,9 @@
+package mk.digital.kmpshowcase.presentation.base.router
+
+interface CopyRouter {
+    fun copyToClipboard(text: String)
+}
+
+expect class CopyRouterImpl : CopyRouter {
+    override fun copyToClipboard(text: String)
+}
