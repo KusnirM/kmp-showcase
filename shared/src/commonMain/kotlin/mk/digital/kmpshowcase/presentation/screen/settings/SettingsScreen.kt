f8b19b63fabc69e503e3510cfc6b844a53c7429d
Miroslav Kusnir <mir.kusnir@gmail.com>
2026-01-22 01:22:20 +0100

    screnwrapper-simplify (#12)

--- shared/src/commonMain/kotlin/mk/digital/kmpshowcase/presentation/screen/settings/SettingsScreen.kt
+++ shared/src/commonMain/kotlin/mk/digital/kmpshowcase/presentation/screen/settings/SettingsScreen.kt
@@ -1,4 +1,4 @@
-package mk.digital.kmpshowcase.presentation.screen.profile
+package mk.digital.kmpshowcase.presentation.screen.settings
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
@@ -7,13 +7,16 @@ import androidx.compose.ui.Modifier
 import mk.digital.kmpshowcase.presentation.component.spacers.ColumnSpacer.Spacer4
 import mk.digital.kmpshowcase.presentation.component.text.bodyLarge.TextBodyLargeNeutral80
 import mk.digital.kmpshowcase.presentation.foundation.space4
+import mk.digital.kmpshowcase.shared.generated.resources.Res
+import mk.digital.kmpshowcase.shared.generated.resources.screen_settings
+import org.jetbrains.compose.resources.stringResource
 
 @Composable
-fun ProfileScreen() {
+fun SettingsScreen() {
     Column {
         Column(Modifier.padding(space4)) {
             Spacer4()
-            TextBodyLargeNeutral80("Profile Body")
+            TextBodyLargeNeutral80(stringResource(Res.string.screen_settings))
         }
     }
 }
