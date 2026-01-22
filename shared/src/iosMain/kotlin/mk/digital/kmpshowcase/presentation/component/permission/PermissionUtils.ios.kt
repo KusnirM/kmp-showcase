d75b1c3e2d43bd027aab4e144c1f71648017a432
Miroslav Kusnir <mir.kusnir@gmail.com>
2026-01-22 20:49:49 +0100

    Camera galery (#22)

--- shared/src/iosMain/kotlin/mk/digital/kmpshowcase/presentation/component/permission/PermissionUtils.ios.kt
+++ shared/src/iosMain/kotlin/mk/digital/kmpshowcase/presentation/component/permission/PermissionUtils.ios.kt
@@ -1,26 +1,5 @@
 package mk.digital.kmpshowcase.presentation.component.permission
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberUpdatedState
-import platform.Foundation.NSNotificationCenter
-import platform.Foundation.NSOperationQueue
-import platform.UIKit.UIApplicationDidBecomeActiveNotification
-
-@Composable
-actual fun RecheckOnResume(onResume: () -> Unit) {
-    val latest by rememberUpdatedState(onResume)
-    DisposableEffect(Unit) {
-        val center = NSNotificationCenter.defaultCenter
-        val observer = center.addObserverForName(
-            name = UIApplicationDidBecomeActiveNotification,
-            `object` = null,
-            queue = NSOperationQueue.mainQueue()
-        ) { _ -> latest() }
-        onDispose { center.removeObserver(observer) }
-    }
-}
 
 actual val galleryRequiresPermission: Boolean
     get() = true
