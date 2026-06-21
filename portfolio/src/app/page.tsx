c870d97258bce76fadf11e4c86419be33461ec9d
Miroslav Kušnír <mir.kusnir@gmail.com>
2026-06-21 11:34:49 +0200

    chore: remove old portfolio copy (extracted to mkdigitalsk/portfolio)

--- portfolio/src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import { AppShowcase } from '@/features/showcase/AppShowcase'
-
-export default function HomePage() {
-  return <AppShowcase />
-}
--- portfolio/src/shared/components/text/TextH4.tsx
+++
@@ -1,6 +0,0 @@
-import { BaseTypography } from './BaseTypography'
-import { type TextInternalProps } from './types'
-
-export function TextH4({ internalSx, ...props }: TextInternalProps) {
-  return <BaseTypography variant="h4" internalSx={internalSx} {...props} />
-}
